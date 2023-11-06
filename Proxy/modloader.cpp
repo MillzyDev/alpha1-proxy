b4cd405f91a8e4ac0ddfa907b6e559917f383090
Millzy <millzyyt08@gmail.com>
2023-11-06 18:27:31 +0000

    call into modloader

--- Proxy/modloader.cpp
+++ Proxy/modloader.cpp
@@ -1,5 +1,17 @@
 ﻿#include "modloader.hpp"
 
+#include <filesystem>
+#include <Windows.h>
+
+typedef void (*modloader_init)();
+
 void init_modloader() {
-    // TODO: the modloader
+    wchar_t current_filename[MAX_PATH];
+    (void)GetModuleFileNameW(nullptr, current_filename, MAX_PATH);
+    const std::filesystem::path module_path = current_filename;
+    const std::filesystem::path alpha1_path = module_path.parent_path() / "alpha1" / "alpha1.dll";
+
+    const HMODULE alpha1_handle = LoadLibraryW(alpha1_path.c_str());
+    const auto alpha1_init_proc = reinterpret_cast<modloader_init>(GetProcAddress(alpha1_handle, "modloader_init"));
+    alpha1_init_proc();
 }
